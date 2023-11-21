eaea5d98f4e44642dbcc60ca1959bd59bbf04413
Sheru29102003 <yuvanthcsc@gmail.com>
2023-11-21 18:51:28 +0530

    first commit

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,20 +1,14 @@
-import { Models } from "appwrite";
-import { Link } from "react-router-dom";
-
-import { PostStats } from "@/components/shared";
-import { useUserContext } from "@/context/AuthContext";
+import { useUserContext } from '@/context/AuthContext'
+import { Models } from 'appwrite'
+import { Link } from 'react-router-dom'
 
 type GridPostListProps = {
   posts: Models.Document[];
   showUser?: boolean;
   showStats?: boolean;
-};
+}
 
-const GridPostList = ({
-  posts,
-  showUser = true,
-  showStats = true,
-}: GridPostListProps) => {
+const GridPostList = ({ posts, showUser = true, showStats = true, }: GridPostListProps) => {
   const { user } = useUserContext();
 
   return (
@@ -33,22 +27,18 @@ const GridPostList = ({
             {showUser && (
               <div className="flex items-center justify-start gap-2 flex-1">
                 <img
-                  src={
-                    post.creator.imageUrl ||
-                    "/assets/icons/profile-placeholder.svg"
-                  }
+                  src={post.creator.imageUrl}
                   alt="creator"
                   className="w-8 h-8 rounded-full"
                 />
                 <p className="line-clamp-1">{post.creator.name}</p>
               </div>
             )}
-            {showStats && <PostStats post={post} userId={user.id} />}
           </div>
         </li>
       ))}
     </ul>
-  );
-};
+  )
+}
 
-export default GridPostList;
+export default GridPostList
